2a5a63859e6391ca0a7e31af625e65d2a9a9d736
Amar Diyab <diyabamar3043@gmail.com>
2023-01-28 04:19:56 +0530

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
+import Calendar from './pages/Calendar';
+import Kanban from './pages/Kanban';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>} >
-        </Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/calendar' element={<Calendar />} />
+        <Route path='/kanban' element={<Kanban />} />
       </Routes>
     </Router>
   )
