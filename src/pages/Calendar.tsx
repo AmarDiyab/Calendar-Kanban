cfe759c735bc21ce9877f7a1a97b1b82ad30886e
Amar Diyab <diyabamar3043@gmail.com>
2023-01-29 13:34:34 +0530

    Completed calendar page

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop }
+    from '@syncfusion/ej2-react-schedule';
 
+import { scheduleData } from '../data/dummy-data';
+import Header from '../components/Header';
+
+ 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
+    return (
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white  rounded-3x1">
+            <Header title='Calendar' />
+            <ScheduleComponent
+                height='650px'
+                eventSettings={{ dataSource: scheduleData }}
+                selectedDate={new Date(2023, 0, 1)} >
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+            </ScheduleComponent>
+        </div>
+    )
 }
 
 export default Calendar
