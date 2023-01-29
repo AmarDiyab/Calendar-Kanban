6e5e2e1f369a90e8cb50e04d12a0313d98a59c31
Amar Diyab <diyabamar3043@gmail.com>
2023-01-29 14:37:52 +0530

    Updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Link } from 'react-router-dom'
 const Home = () => {
     return (
         <div>
-            <Link to={'/calendar'}>
+            <Link to={'/calendar'} className="p-5 text-green-400">
                 <h1>Calendar</h1>
             </Link>
-            <Link to={'/kanban'}>
+            <Link to={'/kanban'} className="p-5 text-green-400">
                 <h1>Kanban</h1>
             </Link>
         </div>
